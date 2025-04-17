e6291f2d71e3d1059d0f99c22639c757e3b9ba40
Mark Heckler <mark.heckler@gmail.com>
2025-04-16 19:03:00 -0500

    Polish REST comms

--- src/main/java/com/thehecklers/BlogEditAI/BlogEditController.java
+++ src/main/java/com/thehecklers/BlogEditAI/BlogEditController.java
@@ -1,6 +1,7 @@
 package com.thehecklers.BlogEditAI;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,8 @@ public class BlogEditController {
         return "Editor service is alive and well.";
     }
 
-    @GetMapping("/api/edit")
+    // GET would be more appropriate since this is idempotent, but POST is used since GET has 2048 char limit
+    @PostMapping("/api/edit")
     public DraftCritique editDraft(@RequestBody DraftRequestSpec draftRequestSpec) {
         return editorService.critiqueDraft(draftRequestSpec);
     }
